a76eff44d673ad2014810682b6c47ad411efccc7
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-14 08:16:44 +0900

    터치 드레그에 맞게 Controller 이미지를 움직이는 기능추가

--- VirtualJoystick/Assets/Scripts/VirtualJoystick.cs
+++ VirtualJoystick/Assets/Scripts/VirtualJoystick.cs
@@ -4,20 +4,38 @@ using UnityEngine.UI;
 
 public class VirtualJoystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
+    public Vector2 TouchPosition => _touchPosition;
+    public float Horizontal => _touchAxis.x;
+    public float Vertical => _touchAxis.y;
+
+    [SerializeField] private RectTransform _rectTransform;
+
+    [Header("Image")]
     [SerializeField] private Image _background;
+    [SerializeField] private Image _controller;
+
+    private Vector2 _touchPosition;
+    private Vector2 _touchAxis;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Touch Begin : " + eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Touch & Drag : " + eventData);
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform, eventData.position, eventData.pressEventCamera, out _touchPosition))
+        {
+            _touchAxis.x = (_touchPosition.x / (_rectTransform.sizeDelta.x * 0.5f));
+            _touchAxis.y = (_touchPosition.y / (_rectTransform.sizeDelta.y * 0.5f));
+            _touchAxis = (_touchAxis.magnitude > 1.0f) ? _touchAxis.normalized : _touchAxis;
+
+            _controller.rectTransform.anchoredPosition = new Vector2(_touchAxis.x * _rectTransform.sizeDelta.x * 0.5f, _touchAxis.y * _rectTransform.sizeDelta.y * 0.5f);
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Touch Ended : " + eventData);
+        _controller.rectTransform.anchoredPosition = Vector2.zero;
+        _touchPosition = Vector2.zero;
     }
 }
