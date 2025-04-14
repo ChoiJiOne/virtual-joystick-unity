3bf5d0e389cac3ed32bd4e7854da4529ea35cc2f
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-14 21:05:57 +0900

    이동 속력을 인스펙터 창에서 제어할 수 있도록 수정

--- VirtualJoystick/Assets/Scripts/PlayerController.cs
+++ VirtualJoystick/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private VirtualJoystick _virtualJoystick;
+    [SerializeField] private float _moveSpeed;
 
     private Vector3 _moveDirection = Vector3.zero;
-    private float _moveSpeed = 1.0f;
 
     private void Update()
     {
         _moveDirection.x = _virtualJoystick.Horizontal;
         _moveDirection.y = _virtualJoystick.Vertical;
-
+        
         transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
     }
 }
