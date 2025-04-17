4b72979ab4f1fa8b73b6d46af3ef17d91012c256
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-17 20:40:17 +0900

    스프라이트를 X축으로 뒤집는 기능 임시 삭제

--- VirtualJoystick/Assets/Scripts/PlayerController.cs
+++ VirtualJoystick/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private SpriteRenderer _spriteRenderer;
 
     private bool _isRun = false;
-    private bool _isDirtyDirection = false;
     private Vector3 _moveDirection = Vector3.zero;
 
     private void Update()
@@ -17,35 +16,15 @@ public class PlayerController : MonoBehaviour
 
         transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
 
-        UpdateSpriteRendererFlip();
         UpdateAnimationState();
     }
 
     private void UpdateMoveDirection()
     {
-        float horizontal = _virtualJoystick.Horizontal;
-        float vertical = _virtualJoystick.Vertical;
-
-        if (_moveDirection.x != horizontal || _moveDirection.y != vertical)
-        {
-            _isDirtyDirection = true;
-        }
-
-        _moveDirection.x = horizontal;
-        _moveDirection.y = vertical;
+        _moveDirection.x = _virtualJoystick.Horizontal;
+        _moveDirection.y = _virtualJoystick.Vertical;
     }
-
-    private void UpdateSpriteRendererFlip()
-    {
-        if (!_isDirtyDirection)
-        {
-            return;
-        }
-
-        _spriteRenderer.flipX = _moveDirection.x < 0.0f;
-        _isDirtyDirection = false;
-    }
-
+    
     private void UpdateAnimationState()
     {
         if (_isRun && _moveDirection.x == 0.0f && _moveDirection.y == 0.0f)
