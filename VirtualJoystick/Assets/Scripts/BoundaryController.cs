260d19bc5738495f4947a2b62c497428c76c1730
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-18 21:23:02 +0900

    플레이어가 카메라 영역 밖으로 나가지 못하는 기능 추가

--- VirtualJoystick/Assets/Scripts/BoundaryController.cs
+++ VirtualJoystick/Assets/Scripts/BoundaryController.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 public class BoundaryController : MonoBehaviour
 {
     [SerializeField] private Camera _mainCamera;
+    [SerializeField] private GameObject _targetObject;
 
-    private float _cameraWidth = 0.0f;
-    private float _cameraHeight = 0.0f;
-    
+    private Bounds _bounds;
+
     private void Awake()
     {
-        _cameraHeight = _mainCamera.orthographicSize * 2.0f;
-        _cameraWidth = _cameraHeight * _mainCamera.aspect;
+        float cameraHeight = _mainCamera.orthographicSize * 2.0f;
+        float cameraWidth = cameraHeight * _mainCamera.aspect;
+
+        _bounds = new Bounds(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(cameraWidth, cameraHeight, 0.0f));
+    }
+
+    private void Update()
+    {
+        Vector3 targetPosition = _targetObject.transform.position;
+        if (_bounds.Contains(targetPosition))
+        {
+            return;
+        }
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, _bounds.min.x, _bounds.max.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, _bounds.min.y, _bounds.max.y);
+
+        _targetObject.transform.position = targetPosition;
     }
 }
